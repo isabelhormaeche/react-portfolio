cbcf96eb7ebb5bd2c83aac2b4b913d96873c6d85
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-06-24 18:45:53 +0200

    Created a portafolio container component and a portfolio item functional component

---
+++ src/components/portfolio/portfolio-item.js
@@ -0,0 +1,9 @@
+import React from "react";
+// you just want to render content, pass it data, image, title, link, etc "Dump component"
+export default function() {
+    return (
+        <div>
+            <h3>Portfolio Item</h3>
+        </div>
+    )
+}
