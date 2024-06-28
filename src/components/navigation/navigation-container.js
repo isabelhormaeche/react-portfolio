49a267104a128a294dafe2bee99e191ff9e0b2a9
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-06-28 20:58:02 +0200

    Added React conditional and use ternary operatrs in jsx code

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,28 @@
+import React, { Component} from "react";
+
+
+
+
+
+export default class NavigationContainer extends Component {
+    constructor() {
+        super();
+    }
+
+    
+
+
+    render(){
+        return (
+            <div>
+               
+                <button>Home</button>
+                <button>About</button>
+                <button>Contact</button>
+                <button>Blog</button>
+                {true ? <button>Add Blog</button> : null }
+            </div>
+        );
+    }
+
+}
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -8,6 +8,7 @@ export default class PortfolioContainer extends Component {
     
     this.state = {
         pageTitle: "Welcome to my portfolio",
+        isLoading: false,
         data: [
             {title:"Quip", category: "eCommerce" }, 
             {title: "Evenbrite", category: "Scheduling" },
@@ -38,6 +39,9 @@ export default class PortfolioContainer extends Component {
 
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>
+        }
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
