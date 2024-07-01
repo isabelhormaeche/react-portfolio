485c7e14038799a504b2eb39b2588b68c26d11f0
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-01 23:31:08 +0200

    Added logging_data_from_an_api

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -47,7 +47,7 @@ export default class PortfolioContainer extends Component {
 
     portfolioItems() {
     return this.state.data.map(item => {
-        console.log("item data", item);
+        console.log("portfolio item", item);
         return (<PortfolioItem 
         key={item.id} 
         title= {item.name}
