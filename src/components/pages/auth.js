5f6971b53438b9f1ce18efd5835e23e42f55d406
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-04 19:00:35 +0200

    How to Import Static Images in React

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,12 +1,22 @@
 import React, { Component} from "react";
-import { NavLink} from "react-router-dom";
+import loginImg from "../../../static/assets/images/auth/login.jpg"
+
 
 
 export default class Auth extends Component {
     render(){
         return (
-            <div>
-                Auth....
+            <div className="auth-page-wrapper">
+                <div 
+                className="left-column" 
+                style={{
+                    backgroundImage: `url(${loginImg})`
+                }}
+                />
+               
+                <div className="right-column">
+                <h1>Login component goes here...</h1>
+                </div>
             </div>
         );
     }
