0b898b774161a10ba3c2e9e7efe5a638d7f56894
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-04 18:18:58 +0200

    Overview of Authentication Behavior

---
+++ src/components/pages/auth.js
@@ -0,0 +1,13 @@
+import React, { Component} from "react";
+import { NavLink} from "react-router-dom";
+
+
+export default class Auth extends Component {
+    render(){
+        return (
+            <div>
+                Auth....
+            </div>
+        );
+    }
+}
