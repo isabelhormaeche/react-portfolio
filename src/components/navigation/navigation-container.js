616a8fe254e92e679b93e57c60e8160ab0aab5ad
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-10 13:39:20 +0200

    Finalizing logout with higher order component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import { withRouter } from "react-router";
 import { NavLink} from "react-router-dom";
 
 
@@ -11,6 +13,22 @@ const NavigationComponent = (props) => {
             </NavLink>
             </div>
         );
+    };
+
+    const handleSignOut = () => {
+        axios
+            .delete("https://api.devcamp.space/logout", {
+            withCredentials: true})
+            .then(response =>{
+                if (response.status === 200){
+                    props.history.push("/");
+                    props.handleSuccessfulLogout();
+                }
+                return response.data;
+            })
+            .catch(error =>{
+                console.log("Error signing out", error);
+            });
     };
 
         return (
@@ -40,11 +58,15 @@ const NavigationComponent = (props) => {
 
                     
                 </div>
-                
-                <div className="right-side">ISABEL HORMAECHE</div>
+                <div className="right-side">
+                    ISABEL HORMAECHE
+                    {props.loggedInStatus === "LOGGED_IN" ? (
+                        <a onClick={handleSignOut}>Sign Out</a>
+                        ) : null}
+                </div>
 
             </div>
         );
     }
 
-export default NavigationComponent;
+export default withRouter(NavigationComponent);
