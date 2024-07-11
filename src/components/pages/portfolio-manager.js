6411c5afd2a74f5985a042937a1bf7507e54c05d
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-11 12:58:43 +0200

    Populating_portfolio_manager_state_with_api_call_data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class PortfolioManager extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            portfolioItems: []
+        };
+    }
+
+    getPortfolioItems() {
+        axios
+          .get("https://isabelhormaeche.devcamp.space/portfolio/portfolio_items", {
+            withCredentials: true
+            })
+          .then(response => {
+
+             console.log("response from get portfolio items", response);
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            });
+          })
+          .catch(error => {
+            console.log("Error in getPortfolioItems", error);
+          });
+    }
+    componentDidMount() {
+        this.getPortfolioItems();
+       }
+
+
     render() {
         return (
             <div className="portfolio-manager-wrapper">
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -6,7 +6,7 @@ export default class PortfolioItem extends Component {
         super(props);
 
         this.state = {
-            portfolItemClass: ""
+            portfolioItemClass: ""
         };
     }
 
