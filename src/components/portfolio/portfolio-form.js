ed7f768153b11092a600f9aca41db1fb008f9467
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-13 20:42:46 +0200

    Select Tag Dropdown

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -91,20 +91,22 @@ export default class PortfolioForm extends Component {
           onChange={this.handleChange}
           />
         
-          <input
-          type="text"
+          <select
           name="category" 
-          placeholder="Category"
           value={this.state.category}
           onChange={this.handleChange}
-          />
+          >
+            <option value="eCommerce">eCommerce</option>
+            <option value="Scheduling">Scheduling</option>
+            <option value="Enterprise">Enterprise</option>
+          </select>
         </div>
 
         <div>
             <input
             type="text"
             name="description" 
-            placeholder="description"
+            placeholder="Description"
             value={this.state.description}
             onChange={this.handleChange}
             />
