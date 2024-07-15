ffd33f7dc428766e0a3c63f4f9075a245e58f777
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-07-15 23:52:44 +0200

    Integrating React Dropzone Component into the Portfolio Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
+
+import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
+import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
 export default class PortfolioForm extends Component {
     constructor(props) {
@@ -18,6 +22,23 @@ export default class PortfolioForm extends Component {
 
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
+      this.componentConfig = this.componentConfig.bind(this);
+      this.djsConfig = this.djsConfig.bind(this);
+    }
+
+    componentConfig() {
+      return {
+        iconFiletypes:[".jpeg", ".png"],
+        showFiletypeIcon: true,
+        postUrl:"https://httpbin.org/post" //mocking a Url
+      }
+    }
+
+    djsConfig() {
+      return {
+        addRemoveLinks: true,
+        maxfiles: 1
+      };
     }
 
     buildForm() {
@@ -112,6 +133,13 @@ export default class PortfolioForm extends Component {
             value={this.state.description}
             onChange={this.handleChange}
             />
+        </div>
+
+        <div className="image-uploaders">
+          <DropzoneComponent
+            config={this.componentConfig()}
+            dsjConfig={this.djsConfig()}
+         />
         </div>
 
         <div>
