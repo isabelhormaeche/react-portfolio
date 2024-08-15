63c0e45da3f475db620f9f25cbd589c129527162
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-15 13:39:52 +0200

    I_03_calling_blog_api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,10 +1,40 @@
 import React , { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 class Blog extends Component {
     constructor() {
         super();
+
+        this.tate = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this);
     }
+
+    getBlogItems() {
+        axios.get("https://isabelhormaeche.devcamp.space/portfolio/portfolio_blogs" , {
+          withCredentials:true
+        })
+        .then(response => {
+            //console.log("response", response);
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        })
+        .catch(error => {
+            console.log("getBlogItems error", error);
+        });
+    }
+
+    componentWillMount(){
+        this.getBlogItems();
+    }
+
+
+
+
     render() {
         return (
             <div>
