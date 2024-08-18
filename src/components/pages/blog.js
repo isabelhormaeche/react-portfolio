81c25b905fc1b33d338848b1213460ae1f70a4f9
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-18 22:39:10 +0200

    I_14_implement_animated_data_loading_icon_in_react (1)

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React , { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
 
@@ -10,7 +11,8 @@ class Blog extends Component {
         this.state = {
             blogItems: [],
             totalCount: 0,
-            currentPage: 0
+            currentPage: 0,
+            isLoading:true
         };
 
         this.getBlogItems = this.getBlogItems.bind(this);
@@ -54,7 +56,8 @@ class Blog extends Component {
             //debugger;
             this.setState({
                 blogItems: response.data.portfolio_blogs,
-                totalCount: response.data.meta.total_records
+                totalCount: response.data.meta.total_records,
+                isLoading: false
             })
         })
         .catch(error => {
@@ -78,6 +81,12 @@ class Blog extends Component {
                 <div className="content-container">
                 {blogRecords}
                 </div>
+
+                {this.state.isLoading ? (
+                    <div className="content-loader">
+                        <FontAwesomeIcon icon="spinner" spin />
+                    </div>
+                ) : null}
             </div>
           );
     }
