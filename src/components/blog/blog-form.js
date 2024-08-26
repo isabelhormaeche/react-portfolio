ed79d78ff37870f972bc66835500aefa00eee0f1
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-26 09:45:09 +0200

    I_40_configuring_dropzone_for_blog_featured_image

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -10,14 +11,40 @@ export default class BlogForm extends Component {
         this.state = {
             title: "",
             blog_status: "",
-            content: ""
+            content: "",
+            featured_image:""
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+        // DropzoneComponent - three configuration functions:
+        this.componentConfig = this.componentConfig.bind(this);
+        this.djsConfig = this.djsConfig.bind(this);
+        this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
 
     }
+
+    componentConfig() {
+      return {
+        iconFiletypes: [".jpg", ".png"],
+        showFiletypeIcon: true,
+        postUrl: "https://httpbin.org/post" // it´ll mimic like it's being sent --> little animation adds the check mark if the file is valid
+      };
+    }
+
+    djsConfig() {
+      return {
+        addRemoveLinks: true,
+        maxFiles: 1
+      };
+    }
+
+    handleFeaturedImageDrop() {
+      return {
+        addedfile: file => this.setState({ featured_image: file })
+      };
+  }
 
     handleRichTextEditorChange(content) {  // we´ll pass string value for content
       
@@ -95,6 +122,16 @@ export default class BlogForm extends Component {
           <RichTextEditor 
            handleRichTextEditorChange = {this.handleRichTextEditorChange}
           />
+        </div>
+
+        <div className="image-uploaders">
+          <DropzoneComponent
+            config={this.componentConfig()} // added () cause we want to run right away
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleFeaturedImageDrop()}
+          >
+            <div className="dz-message">Featured Image</div> 
+          </DropzoneComponent>
         </div>
 
         <button className="btn" >Save</button>
