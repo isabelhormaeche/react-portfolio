e560abcfa8595d5f8fb6e5f3da51d7731a674dd3
Isabel Hormaeche <isabel.hormaeche@gmail.com>
2024-08-29 15:28:17 +0200

    J_06_portfolio_detail_styles

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 
 
-
-
 export default class PortfolioDetail extends Component {
     constructor(props) {
         super(props);
@@ -48,10 +46,36 @@ export default class PortfolioDetail extends Component {
             url,
             } = this.state.portfolioItem;
 
+            // a style object listing key:value pairs, is not cCSS, don´t put "";""
+            const bannerStyles = {
+                backgroundImage: "url(" + banner_image_url + ")",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center center"
+              };
+          
+              const logoStyles = {
+                width: "200px"
+              };
+
         return (
-                <div>
-                    <h2>{name}</h2>
-                    <p>{description}</p>
+                <div className= "portfolio-detail-wrapper">
+                    <div className="banner" style={bannerStyles}>
+                        {/* //wil also use some inline styles to be dynamic */}
+                        <img src={logo_url} style={logoStyles}/>
+                    </div>
+
+                    <div className="portfolio-detail-description-wrapper">
+                        <div className="description">{description}</div>
+                    </div>
+
+                    <div className="bottom-content-wrapper">
+                        <a  href={url} className="site-link" target="_blank" 
+                        // here TARGET opens up a new tab, won't take them away from the portfolio itself
+                        >
+                            Visit {name}
+                        </a>
+                    </div>
                 </div>
             );
     }
